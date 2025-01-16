02c62231db02fe8ed1f4742bd3d1b993a53b8bb5
Wemerson <wemersoncontas22@gmail.com>
2025-01-16 13:17:50 -0300

    feat: create city table.

---
+++ mysql/createCityTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS city (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    city_name VARCHAR(255) NOT NULL,
+    state_id int unsigned NOT NULL,
+    area DECIMAL(10,2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states (id)
+);
