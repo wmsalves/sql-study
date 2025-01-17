713eb56fc79914294d991fb92fe91fb00d1c4131
Wemerson <wemersoncontas22@gmail.com>
2025-01-17 09:53:30 -0300

    feat: insert into mayor table.

---
+++ mysql/insertMayorTable.sql
@@ -0,0 +1,20 @@
+SELECT * FROM cities;
+
+INSERT INTO mayors
+    (mayors_name, city_id)
+VALUES
+    ('John Doe', 2),
+    ('Jane Smith', 3),
+    ('Carlos Souza', NULL);
+
+SELECT * FROM mayors;
+
+INSERT INTO mayors
+    (mayors_name, city_id)
+VALUES
+    ('Lucas Pereira', NULL);
+
+INSERT INTO mayors
+    (mayors_name, city_id)
+VALUES
+    ('Cláudia Rodrigues', 3);
