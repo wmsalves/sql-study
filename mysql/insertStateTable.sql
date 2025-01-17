9aaaaced6c9669136e51a88d60cb45a9f814bb32
Wemerson <wemersoncontas22@gmail.com>
2025-01-17 09:48:23 -0300

    feat: insert into state table.

---
+++ mysql/insertStateTable.sql
@@ -0,0 +1,11 @@
+insert into states 
+    (state_name, state_abbreviation, region, population)
+VALUES 
+    ('Alabama', 'AL', 'South', 5.03);
+
+INSERT INTO states
+    (state_name, state_abbreviation, region, population)
+VALUES  
+    ('Alaska', 'AK', 'West', 0.73),
+    ('Arizona', 'AZ', 'West', 7.28),
+    ('Arkansas', 'AR', 'South', 3.03);
